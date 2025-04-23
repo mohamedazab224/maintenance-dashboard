b419fab316faf1756b4687e9924554efe85d7ce7
Mohamed Azab <159525429+mohamedazab224@users.noreply.github.com>
2025-04-23 09:55:53 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,14 @@ import type { ClassValue } from 'clsx'
 import type { Ref } from 'vue'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { createClient } from '@supabase/supabase-js'
+
+// إعداد متغيرات البيئة
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
+
+// إنشاء عميل Supabase
+export const supabase = createClient (SUPABASE_URL, SUPABASE_KEY)
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
